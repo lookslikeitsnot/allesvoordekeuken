ff28cd6b46f58504bd9aef705290bc400cd280f5
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-02-14 10:44:20 +0100

    met ThreadLocal

--- src/main/java/be/vdab/filters/JPAFilter.java
+++ src/main/java/be/vdab/filters/JPAFilter.java
@@ -17,6 +17,7 @@ import javax.servlet.annotation.WebFilter;
 public class JPAFilter implements Filter {
 	private static final EntityManagerFactory entityManagerFactory = Persistence
 			.createEntityManagerFactory("allesvoordekeuken");
+	private static final ThreadLocal<EntityManager> entityManagers = new ThreadLocal<>();
 
 	@Override
 	public void init(FilterConfig config) throws ServletException {
@@ -25,9 +26,15 @@ public class JPAFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		chain.doFilter(request, response);
+			throws IOException, ServletException {
+		entityManagers.set(entityManagerFactory.createEntityManager());
+		try {
+			request.setCharacterEncoding("UTF-8");
+			chain.doFilter(request, response);
+		} finally {
+			entityManagers.get().close();
+			entityManagers.remove();
+		}
 	}
 
 	@Override
@@ -36,6 +43,6 @@ public class JPAFilter implements Filter {
 	}
 
 	public static EntityManager getEntityManager() {
-		return entityManagerFactory.createEntityManager();
+		return entityManagers.get();
 	}
 }
